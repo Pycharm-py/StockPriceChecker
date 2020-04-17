2d513dba7725ed83ea242687835473aff94a4314
Pycharm-py <61084121+Pycharm-py@users.noreply.github.com>
2020-04-17 16:10:11 +0200

    Create Checker.py

---
+++ Checker.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+try:
+    while True:
+        # go to that web page
+        req = requests.get("https://finance.yahoo.com/quote/%5EIXIC?p=")
+        # get the code of page
+        content = req.content
+        # parse the code of page
+        soup = BeautifulSoup(content, "lxml")
+        # get the price
+        price = soup.find("div", {"class": "My(6px) Pos(r) smartphone_Mt(6px)"})
+        print("price =", price.find("span").text)
+except Exception:
+    pass
